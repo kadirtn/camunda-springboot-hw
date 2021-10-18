236da99bb26da519a7f27035e793ae3fdeacc996
Kadir Tuna <kadir.tuna@hmb.gov.tr>
2021-10-18 09:44:29 +0300

    web disable edildi

--- src/main/java/tr/com/ketuna/camunda/handler/rating/CandidateRatingHandler.java
+++ src/main/java/tr/com/ketuna/camunda/handler/rating/CandidateRatingHandler.java
@@ -1,29 +1,21 @@
 package tr.com.ketuna.camunda.handler.rating;
-import org.camunda.bpm.client.spring.annotation.ExternalTaskSubscription;
-import org.camunda.bpm.client.task.ExternalTask;
-import org.camunda.bpm.client.task.ExternalTaskHandler;
-import org.camunda.bpm.client.task.ExternalTaskService;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.springframework.stereotype.Component;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @Component
-@ExternalTaskSubscription("candidateRating")
-public class CandidateRatingHandler implements ExternalTaskHandler {
+public class CandidateRatingHandler implements JavaDelegate {
 
     Logger logger = Logger.getLogger("CandidateRatingHandler");
+
     @Override
-    public void execute(ExternalTask externalTask, ExternalTaskService externalTaskService) {
+    public void execute(DelegateExecution delegateExecution) {
 
-        int candidateScore = externalTask.getVariable("candidateScore");
-
-        //Logic to process based on rating goes here
-
-        externalTaskService.complete(externalTask);
+        int candidateScore = (int) delegateExecution.getVariable("candidateScore");
 
         logger.log(Level.INFO, "Processing candidate Score received {0}", new Object[]{ candidateScore});
-
     }
-
 }
--- src/main/java/tr/com/ketuna/camunda/handler/rating/GenerateRatingHandler.java
+++ src/main/java/tr/com/ketuna/camunda/handler/rating/GenerateRatingHandler.java
@@ -1,35 +1,28 @@
 package tr.com.ketuna.camunda.handler.rating;
 
-import org.camunda.bpm.client.spring.annotation.ExternalTaskSubscription;
-import org.camunda.bpm.client.task.ExternalTask;
-import org.camunda.bpm.client.task.ExternalTaskHandler;
-import org.camunda.bpm.client.task.ExternalTaskService;
-import org.camunda.bpm.engine.variable.VariableMap;
-import org.camunda.bpm.engine.variable.Variables;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.springframework.stereotype.Component;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @Component
-@ExternalTaskSubscription("ratingProvider")
-public class GenerateRatingHandler implements ExternalTaskHandler {
+public class GenerateRatingHandler implements JavaDelegate {
 
     Logger logger = Logger.getLogger("GenerateRatingHandler");
+
+
     @Override
-    public void execute(ExternalTask externalTask, ExternalTaskService externalTaskService) {
+    public void execute(DelegateExecution delegateExecution) {
 
         String candidateId = "IN-" + (Math.random() * 100);
         int candidateScore = (int) (Math.random() * 11);
 
-        VariableMap variables = Variables.createVariables();
-        variables.put("candidateId", candidateId);
-        variables.put("candidateScore", candidateScore);
-
-        externalTaskService.complete(externalTask, variables);
+        delegateExecution.setVariable("candidateId", candidateId);
+        delegateExecution.setVariable("candidateScore", candidateScore);
 
         logger.log(Level.INFO, "NOPE Rating Genrator External task is completed for Candidate {0} with rating {1}", new Object[]{candidateId, candidateScore});
 
     }
-
 }
